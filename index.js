431a85b365982f4d0dbcc10e20b81a3a6645b535
Sunil Chaudhary <sunilchaudhary7789@gmail.com>
2024-09-10 15:50:44 +0530

    updated again

--- index.js
+++ index.js
@@ -1,58 +1,58 @@
-
 let searchInput = document.getElementById("searchInput");
 const resultDiv = document.getElementById("result");
-let timeout 
+let timeout;
 console.log(resultDiv);
 let data = [
-    {name:"sunil"},
-    {name:"sun"},
-   { name:"Rajesh"},
-   {name:"Golu"}
-]
+    {name:"ankit"},
+    {name:"bima"},
+    {name:"cima"},
+    {name:"don"},
+  { name: "sunil" },
+  { name: "sun" },
+  { name: "Rajesh" },
+  { name: "Golu" },
+];
 
-function simulateApiCall(query){
-  return new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-        let filterData = data.filter((el,i)=>el.name.toLowerCase().includes(query.toLowerCase()));
-        resolve(filterData);
-    },1000)
-  })
+function simulateApiCall(query) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let filterData = data.filter((el, i) =>
+        el.name.toLowerCase().includes(query.toLowerCase())
+      );
+      resolve(filterData);
+    }, 1000);
+  });
 }
 
-function displayResult(result){
-resultDiv.innerHTML = ""
+function displayResult(result) {
+  resultDiv.innerHTML = "";
 
-if (result.length === 0) {
-    const noResultItem = document.createElement('p');
-    noResultItem.textContent = 'No results found';
+  if (result.length === 0) {
+    const noResultItem = document.createElement("p");
+    noResultItem.textContent = "No results found";
     resultDiv.appendChild(noResultItem);
     return;
   }
-    if(result.length>0){
-        result.forEach((el)=>{
-            let resultItem = document.createElement('p');
-            resultItem.textContent=el.name
-            resultDiv.appendChild(resultItem);
-        })
-    }
-}
 
-
-function debouchSeach (fun,delay){
-  clearTimeout(timeout);
-  timeout = setTimeout(fun,delay);
-  
-
-
-}
-
-searchInput.addEventListener('input', (event) => {
-    const query = event.target.value.trim();
-    if (query.length > 0) {
-        debouchSeach(() => {
-            simulateApiCall(query).then(displayResult);
-      }, 300);  // Delay of 300ms
-    } else {
-      resultDiv.innerHTML = ''; // Clear results if input is empty
-    }
+  result.forEach((el) => {
+    let resultItem = document.createElement("p");
+    resultItem.textContent = el.name;
+    resultDiv.appendChild(resultItem);
   });
+}
+
+function debouchSeach(fun, delay) {
+  clearTimeout(timeout);
+  timeout = setTimeout(fun, delay);
+}
+
+searchInput.addEventListener("input", (event) => {
+  const query = event.target.value.trim();
+  if (query.length > 0) {
+    debouchSeach(() => {
+      simulateApiCall(query).then(displayResult);
+    }, 300);
+  } else {
+    resultDiv.innerHTML = "";
+  }
+});
